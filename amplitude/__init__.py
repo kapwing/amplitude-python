b8390df49231143de269a9d032c2494fbf4c52cb
David Heller <david@ehellers.com>
2020-02-21 14:55:21 -0800

    Changing amplitude interface

--- amplitude/__init__.py
+++ amplitude/__init__.py
@@ -8,63 +8,63 @@ import json
 # Convert Curl queries - such as below to - python:
 #   https://curl.trillworks.com/
 #
-# Example HTTP Curl Query for Amplitude: 
-#   curl --data 'api_key=SOMEIDOFAKIND' --data 'event=[{"user_id":"john_doe@gmail.com", "event_type":"watch_tutorial", "user_properties":{"Cohort":"Test A"}, "country":"United States", "ip":"127.0.0.1", "time":1396381378123}]' https://api.amplitude.com/httpapi
+# Example HTTP Curl Query for Amplitude:
+#   curl --data 'api_key=SOMEIDOFAKIND' --data 'event=[{'user_id':'john_doe@gmail.com', 'event_type':'watch_tutorial', 'user_properties':{'Cohort':'Test A'}, 'country':'United States', 'ip':'127.0.0.1', 'time':1396381378123}]' https://api.amplitude.com/httpapi
 
+DEFAULT_API_URL = 'https://api.amplitude.com/2/httpapi'
 
 class AmplitudeLogger:
-    def __init__(self, api_key, api_uri="https://api.amplitude.com/2/httpapi"):
+    def __init__(self, api_key, api_uri=DEFAULT_API_URL):
         self.api_key = api_key
         self.api_uri = api_uri
-        self.is_logging = True
+        self.enabled = True
 
-    def turn_on_logging(self):
-        self.is_logging = True
+    def _is_none_or_not_str(self, value):
+        return value is None or type(value) is not str
 
-    def turn_off_logging(self):
-        self.is_logging = False
-
-    def _is_None_or_not_str(self, value):
-        if value is None or type(value) is not str:
-            return True
-        
     def create_event(self, **kwargs):
-        event = {}
-        user_id = kwargs.get('user_id', None)
-        device_id = kwargs.get('device_id', None)
+        event = dict()
 
-        if self._is_None_or_not_str(user_id) or self._is_None_or_not_str(device_id):
+        device_id = kwargs.get('device_id', None)
+        event_type = kwargs.get('event_type', None)
+        user_id = kwargs.get('user_id', None)
+
+        if self._is_none_or_not_str(event_type):
             return None
-        else:
-            event["device_id"] = device_id
-            event["user_id"] = user_id
+
+        if self._is_none_or_not_str(user_id) and self._is_none_or_not_str(device_id):
+            return None
+
+        event['event_type'] = event_type
+        event['time'] = int(time.time() * 1000)
+
+        if device_id:
+            event['device_id'] = device_id
+
+        if user_id:
+            event['user_id'] = user_id
 
         user_properties = kwargs.get('user_properties', None)
-        if user_properties is not None and type(user_properties) == dict:
-            event["user_properties"] = user_properties
+        if isinstance(user_properties, dict):
+            event['user_properties'] = user_properties
 
-        event_type = kwargs.get('event_type', None)
-        if self._is_None_or_not_str(event_type):
-            return None
-
-        event["event_type"] = event_type
-        
-        # integer epoch time in milliseconds 
-        event["time"] = int(time.time()*1000)
-        
         event_properties = kwargs.get('event_properties', None)
-        if event_properties is not None and type(event_properties) == dict:
-            event["event_properties"] = event_properties
+        if isinstance(event_properties, dict):
+            event['event_properties'] = event_properties
 
-        event_package = {
-            'api_key': self.api_key,
-            'events': [event]
-        }
+        return event
 
-        return event_package
+    def log_events(self, events):
+        if not (self.enabled and event):
+            return
 
-    def log_event(self, event):
-        if event is not None:
-            if self.is_logging:
-                result = requests.post(self.api_uri, json=event)
-                return result
+        return self.log_events([event])
+
+    def log_events(self, events):
+        if not (self.enabled and event):
+            return
+
+        return requests.post(
+            self.api_uri,
+            json=dict(api_key=self.api_key, events=events)
+        )
