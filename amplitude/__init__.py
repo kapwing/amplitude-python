2f2aeace8be541215a019a65bff97d81f60f9159
David Heller <david@ehellers.com>
2020-02-21 15:06:18 -0800

    Cleaning code

--- amplitude/__init__.py
+++ amplitude/__init__.py
@@ -14,23 +14,20 @@ import json
 DEFAULT_API_URL = 'https://api.amplitude.com/2/httpapi'
 
 class AmplitudeLogger:
-    def __init__(self, api_key, api_uri=DEFAULT_API_URL):
+    def __init__(self, api_key, api_url=DEFAULT_API_URL):
         self.api_key = api_key
-        self.api_uri = api_uri
+        self.api_url = api_url
         self.enabled = True
 
-    def _is_none_or_not_str(self, value):
-        return value is None or type(value) is not str
-
     def create_event(self, **kwargs):
-        device_id = kwargs.get('device_id', None)
-        event_type = kwargs.get('event_type', None)
-        user_id = kwargs.get('user_id', None)
+        device_id = kwargs.get('device_id')
+        event_type = kwargs.get('event_type')
+        user_id = kwargs.get('user_id')
 
-        if self._is_none_or_not_str(event_type):
+        if not isinstance(event_type, str):
             return None
 
-        if self._is_none_or_not_str(user_id) and self._is_none_or_not_str(device_id):
+        if not (isinstance(device_id, str) or isinstance(device_id, str)):
             return None
 
         event = dict(
@@ -45,15 +42,17 @@ class AmplitudeLogger:
             event['user_id'] = user_id
 
         for key in ['event_properties', 'user_properties']:
-            value = kwargs.get(key, None)
-            if isinstance(value, dict):
-                event[key] = value
+            value = kwargs.get(key)
+            if not isinstance(value, dict):
+                continue
+
+            event[key] = value
 
         return event
 
     def log(self, **kwargs):
         event = self.create_event(**kwargs)
-        self.send_event(event)
+        return self.send_event(event)
 
     def send_event(self, event):
         if not (self.enabled and event):
@@ -62,10 +61,10 @@ class AmplitudeLogger:
         return self.send_events([event])
 
     def send_events(self, events):
-        if not (self.enabled and event):
+        if not (self.enabled and events):
             return
 
         return requests.post(
-            self.api_uri,
+            self.api_url,
             json=dict(api_key=self.api_key, events=events)
         )
